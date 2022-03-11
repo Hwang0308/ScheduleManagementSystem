2b0f9e79ac9f8faca6094529c4c2911bc6540d74
Hwang0308 <deanson3723@naver.com>
2022-03-11 14:25:50 +0900

    Delete 'Hello.java' Add 'Menu_list.java'

---
+++ src/Menu_list.java
@@ -0,0 +1,13 @@
+
+public class Menu_list {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add schedule");
+		System.out.println("2. Modify schedule");
+		System.out.println("3. Delete schedule");
+		System.out.println("4. Input schedule importance");
+		System.out.println("5. Show schedule");
+		System.out.println("6. Exit");
+	}
+
+}
