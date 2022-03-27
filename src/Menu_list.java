181cc6c713d3ef4ccdc26bf7e2965e322ab62212
Hwang0308 <deanson3723@naver.com>
2022-03-27 11:48:07 +0900

    Add class AddSchedule.java DeleteSchedule.java EditSchedule.java ShowSchedule.java

--- src/Menu_list.java
+++ src/Menu_list.java
@@ -1,28 +1,51 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Menu_list {
-
-	public static void main(String[] args) {	
+	public static void AddSchedule() {
+		ArrayList<String> ScheduleList = new ArrayList<String>();
+		
+		System.out.print("Month(01 ~ 12) : ");
+		Scanner input = new Scanner(System.in);
+		String month = input.nextLine();
+		ScheduleList.add(month);
+		
+		System.out.print("Day(01 ~ 31) : ");
+		input = new Scanner(System.in);
+		String day = input.nextLine();
+		ScheduleList.add(day);
+		
+		System.out.print("Schedule Title : ");
+		input = new Scanner(System.in);
+		String title = input.nextLine();
+		ScheduleList.add(title);
+		
+		System.out.println("New Schedule!");
+		System.out.println("");
+		System.out.println("Month / day : " + month + "/" + day);
+		System.out.println("Title : " + title);
+		
+		System.out.println(ScheduleList);
+		System.out.println("");
+	}
+	
+	public static void main(String[] args) {
 		while (true) {
 			System.out.println("1. Add schedule");
 			System.out.println("2. Edit schedule");
 			System.out.println("3. Delete schedule");
 			System.out.println("4. Show schedule");
 			System.out.println("5. Exit");
+			System.out.println("-----------------------");
 			
 			Scanner menu = new Scanner(System.in);
 			int menu_list = menu.nextInt();
 			
 			if (menu_list == 1) {
 				System.out.println("Add schedule!");
-				System.out.print("Month(01 ~ 12) : ");
-				Scanner m = new Scanner(System.in);
-				int month = m.nextInt();
-				System.out.print("Day(01 ~ 31) : ");
-				Scanner d = new Scanner(System.in);
-				int day = d.nextInt();
-			
-				System.out.println("New Schedule! " + month + "/" + day);
+				AddSchedule();
+				
 			}
 			else if (menu_list == 2) {
 				System.out.println("Edit schedule!");
